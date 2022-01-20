abaca74090da9525188eb5aca4952d90c018a218
Andrew <andrew.afony@yandex.ru>
2022-01-20 23:47:37 +0300

    add menu to article fragment

--- app/src/main/java/com/myapp/newsapp/presentation/fragments/ArticleFragment.kt
+++ app/src/main/java/com/myapp/newsapp/presentation/fragments/ArticleFragment.kt
@@ -5,9 +5,11 @@ import android.view.View
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.myapp.newsapp.R
 import com.myapp.newsapp.presentation.NewsViewModel
+import kotlinx.android.synthetic.main.activity_news.*
 import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
@@ -26,7 +28,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
                 loadUrl(it)
             }
         }
-
     }
+
 
 }
