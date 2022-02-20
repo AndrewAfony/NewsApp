6f4b97af5399031b6a4168bbb1674aed656a15bc
Andrew <andrew.afony@yandex.ru>
2022-02-20 23:05:40 +0300

    add view binding to adapter of recycler view

--- app/src/main/java/com/myapp/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/myapp/newsapp/adapters/NewsAdapter.kt
@@ -1,19 +1,17 @@
 package com.myapp.newsapp.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.myapp.newsapp.R
+import com.myapp.newsapp.databinding.ItemArticlePreviewBinding
 import com.myapp.newsapp.domain.model.Article
-import kotlinx.android.synthetic.main.item_article_preview.view.*
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
-    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    inner class ArticleViewHolder(val binding: ItemArticlePreviewBinding): RecyclerView.ViewHolder(binding.root)
 
     private val differCallback = object : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
@@ -29,20 +27,22 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         return ArticleViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_article_preview, parent, false)
+            ItemArticlePreviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val article = differ.currentList[position]
-        holder.itemView.apply {
-            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
+        holder.binding.apply {
+            Glide.with(this.root)
+                .load(article.urlToImage)
+                .into(ivArticleImage)
             tvSource.text = article.source?.name
             tvTitle.text = article.title
 //            tvDescription.text = article.description
             tvPublishedAt.text = article.publishedAt
 
-            setOnClickListener {
+            root.setOnClickListener {
                 onItemClickListener?.let {
                     it(article)
                 }
